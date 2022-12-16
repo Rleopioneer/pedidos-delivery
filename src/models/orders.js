62d871e0eb1c52e45f4eefa739a480169e7cbdbc
Ramon Leonardo <ramon.c.leo@hotmail.com>
2022-12-16 11:44:02 -0300

    Create Orders Collection

---
+++ src/models/orders.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+//Creating Schema
+
+const schema = new mongoose.Schema({
+    clientCode: String,
+    productsCode: String,
+    dateCreation: String,
+    status: String
+})
+
+const Model = mongoose.model('orders', schema)
+
+module.exports = Model
