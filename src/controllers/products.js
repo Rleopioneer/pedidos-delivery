c3a61dee3f62db66a0d5f415581768ba71410179
Ramon Leonardo <ramon.c.leo@hotmail.com>
2022-12-16 10:30:22 -0300

    Create controller and GET method for Products

---
+++ src/controllers/products.js
@@ -0,0 +1,76 @@
+const ProductsModel = require('../models/products')
+
+async function get(req, res) {
+
+    const { id } = req.params
+
+    const obj = id ? { _id: id} : null
+
+    const products = await ProductsModel.find(obj)
+
+    res.send(products)
+
+}
+
+/* async function post(req, res) {
+
+    const {
+        name,
+        email,
+        phone,
+        adress,
+    } = req.body
+
+    console.log(req.body)
+
+    const client = new ClientsModel({
+        name,
+        email,
+        phone,
+        adress
+    })
+
+    client.save()
+
+    res.send({
+        message: 'Success'
+    })
+
+} */
+
+/* async function put(req, res) {
+
+    const { id } = req.params
+
+    const client = await ClientsModel.findOneAndUpdate({ _id: id}, req.body, { new: true })
+
+    res.send({
+        message:'Success',
+        client
+    })
+
+} */
+
+/* async function del(req, res) {
+
+    const { id } = req.params
+
+    const del = await ClientsModel.deleteOne({ _id: id})
+
+    const message = del.ok ? 'Success' : 'error'
+
+    res.send({
+        message: 'Success'
+    })
+
+
+} */
+
+
+
+module.exports = {
+    get,
+    //post,
+    //put,
+    //del,
+}
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,17 @@
 const router = require('express').Router()
 const ClientsController = require('../controllers/clients')
+const ProductsController = require('../controllers/products')
 
+//Clients
 router.get('/clients/:id?', ClientsController.get)
 router.post('/clients', ClientsController.post)
 router.put('/clients/:id', ClientsController.put)
 router.delete('/clients/:id', ClientsController.del)
+
+//Products
+router.get('/products/:id?',ProductsController.get)
+//router.post('/clients', ClientsController.post)
+//router.put('/clients/:id', ClientsController.put)
+//router.delete('/clients/:id', ClientsController.del)
 
 module.exports = router
